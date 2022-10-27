f61679d45f550fba2591823eb864d585072b374d
harsha-penugonda <vpenugon@stevens.edu>
2022-10-26 21:05:36 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Home Page of Final Project</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
