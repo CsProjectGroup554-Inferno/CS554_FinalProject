bda9669b9f1bf3789c7fcefa0fac1616d106cd34
Hk6777 <harshilkachhadiya6777@gmail.com>
2022-12-17 11:59:30 -0500

    Change app.js file
    changes in some configuration in socket.to

--- app.js
+++ app.js
@@ -2,59 +2,51 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const cors = require("cors");
+const socket = require("socket.io");
+require("dotenv").config();
+
 const http = require("http").createServer(app);
-const io = require("socket.io")(http, {
-  cors: {
-    origin: "http://localhost:3000",
-  },
-});
-io.use((socket, next) => {
-  const username = socket.handshake.auth.dd;
-  socket.username = username;
-  next();
-});
-io.on("connection", (socket) => {
-  const users = [];
-  for (let [id, socket] of io.of("/").sockets) {
-    users.push({
-      userID: id,
-      username: socket.username,
-      key: id,
-    });
-  }
-  socket.emit("users", users);
-  console.log(users);
-
-  socket.broadcast.emit("user connected", {
-    userID: socket.id,
-    username: socket.username,
-    key: socket.id,
-    self: false,
-  });
-
-  socket.on("private message", ({ content, to }) => {
-    console.log("Content:", content, " To:", to);
-    socket.to(to).emit("private message", {
-      content,
-      from: socket.id,
-    });
-  });
-});
-
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
-
-app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use(cors());
+// Configure Rounte and express
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
+app.use(express.static(path.join(__dirname, "client/build")));
 const configRoutes = require("./routes/index");
 configRoutes(app);
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 const port = process.env.PORT || 4000;
-http.listen(port, () => {
+const server = http.listen(port, () => {
   console.log("Listening on port 4000");
 });
+
+// Configure socket.io
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
+
+
+
+
+
